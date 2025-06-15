5aa42291b758e29f32634047dd919db4469c9522
Saurav_Bhasin <bhasinsaurav9@gmail.com>
2025-06-15 16:50:41 -0400

    Kafka integrated

--- src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
+++ src/main/java/net/engineeringdigest/journalApp/scheduler/UserScheduler.java
@@ -3,9 +3,11 @@ package net.engineeringdigest.journalApp.scheduler;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.enums.Sentiment;
+import net.engineeringdigest.journalApp.model.SentimentData;
 import net.engineeringdigest.journalApp.repository.UserRepositoryImpl;
 import net.engineeringdigest.journalApp.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +26,9 @@ public class UserScheduler {
 
     @Autowired
     private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private KafkaTemplate<String, SentimentData> kafkaTemplate;
 
     @Scheduled(cron = "0 0 9 * * Sun")
     public void fetchUsersAndSendSaMail(){
@@ -46,7 +51,8 @@ public class UserScheduler {
                 }
             }
             if(mostFrequentSentiment != null){
-                emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days ", mostFrequentSentiment.toString());
+                SentimentData sentimentData= SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
+                kafkaTemplate.send("weekly-sentiments", sentimentData.getEmail(), sentimentData);
             }
         }
     }
